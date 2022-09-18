e5e8a495c398537069feee670887f5d1c74e3b66
thomasclarito <43707071+thomasclarito@users.noreply.github.com>
2022-09-17 17:01:16 -0700

    projectile functionality

--- LizzsMansion/Assets/Scripts/projectileScript.cs
+++ LizzsMansion/Assets/Scripts/projectileScript.cs
@@ -4,15 +4,39 @@ using UnityEngine;
 
 public class projectileScript : MonoBehaviour
 {
+    private Transform projPosition;
+    public Transform target;
+    public float speed;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        projPosition = gameObject.transform;
+    }
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Move();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Move()
     {
-        
+        Vector3 moveDirection = (target.position - projPosition.position).normalized;
+        float distance = speed * Time.deltaTime;
+        projPosition.transform.position = projPosition.position + moveDirection * distance;
+        gameObject.transform.position = projPosition.transform.position;
     }
-}
+
+    private void OnTriggerEnter(Collider collision)
+    {
+        print("entered collision");
+        if (collision.gameObject.GetComponent<EnemyController>() != null)
+        {
+            print("damage");
+            Destroy(gameObject);
+        }
+
+    }
+
+}   
